d583206970848a8e5a650ca428ee431748df6432
Sihan-Tao <sihan.tao20@imperial.ac.uk>
2021-08-10 16:08:36 +0800

    Implement some features of the class VideoPlaylist

--- src/main/java/com/google/VideoPlaylist.java
+++ src/main/java/com/google/VideoPlaylist.java
@@ -2,5 +2,12 @@ package com.google;
 
 /** A class used to represent a Playlist */
 class VideoPlaylist {
+  private final String name;
+  public VideoPlaylist(String name) {
+    this.name = name;
+  }
 
+  public String getPlaylistName() {
+    return name;
+  }
 }
