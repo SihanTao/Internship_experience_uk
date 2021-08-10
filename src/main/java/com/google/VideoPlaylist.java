71097d2a145e27719077f02846e249364b22286a
Sihan-Tao <sihan.tao20@imperial.ac.uk>
2021-08-10 16:27:47 +0800

    Add a videoIdList in the class VideoPlaylist

--- src/main/java/com/google/VideoPlaylist.java
+++ src/main/java/com/google/VideoPlaylist.java
@@ -1,13 +1,28 @@
 package com.google;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /** A class used to represent a Playlist */
 class VideoPlaylist {
+
   private final String name;
+  private final List<String> videoidList;
+
   public VideoPlaylist(String name) {
     this.name = name;
+    this.videoidList = new ArrayList<>();
   }
 
   public String getPlaylistName() {
     return name;
+  }
+
+  /**
+   * Add a video to a playlist.
+   * @param videoId : the unique id for the video to add
+   */
+  public void add_to_videoIdList(String videoId) {
+    this.videoidList.add(videoId);
   }
 }
